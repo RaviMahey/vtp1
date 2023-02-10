b62c3c6b2f41de9327840a094ed466e6631c7a83
RaviMahey <ravi752696@gmail.com>
2023-02-10 08:44:35 +0530

    fourth commit

--- app.js
+++ app.js
@@ -13,15 +13,21 @@ app.use(express.json());
 app.use(require('./conn'));
 app.use(require('./auth'));
 // console.log("yes");
+app.use(express.static(path.join(__dirname,'./client/build')));
+app.get('*',(req,res)=>{
+  res.sendFile(path.join(__dirname,'./client/build/index.html'));
+});
+
 app.get('/logout',(req,res)=>{
   console.log("reached");
   res.clearCookie("jwtoken",{path:'/'});
   res.status(200).send("user logout");
   // req.user.save();
-})
-if(process.env.NODE_ENV==="production"){
-  app.use(express.static("client/build"));
-}
+});
+// if(process.env.NODE_ENV==="production"){
+//   app.use(express.static("client/build"));
+//   app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
+// }
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
